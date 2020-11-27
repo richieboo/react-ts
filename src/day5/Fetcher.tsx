8a0d3dc34330ff70c96c5a97f84dde668bd8274b
Richie Madden <richie@richiemadden.com>
2020-11-27 17:25:05 -0500

    Update Fetcher.tsx

--- src/day5/Fetcher.tsx
+++ src/day5/Fetcher.tsx
@@ -9,18 +9,19 @@ const Fetcher = ()=>{
     useEffect(()=>{
         setNum(0);
     },[]);
+    
+    //I learned that setResult only changes state for the next render, now when we need it if not re-rendering.
+    useEffect(() => {
+        fetch(`http://numbersapi.com/${num}/trivia`).then((data) => {
+            data.text().then((returned) => {
+                setResult(returned);
+            })
+        });
+    });
 
     const handleChange = (event: React.KeyboardEvent<HTMLInputElement>)=>{
         setNum(parseInt(event.currentTarget.value));
         console.log(`http://numbersapi.com/${num}/trivia`);
-        
-        fetch(`http://numbersapi.com/${num}/trivia`).then((data)=>{
-            
-            data.text().then((returned)=>{
-                
-                setResult(returned);
-            });
-        });
     }
     
 
